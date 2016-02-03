eff0261c4208e7fffaf70903405e2c854dbf451c
Qiang Wang <wangqiang2015@ia.ac.cn>
2016-02-03 08:54:52 +0800

    my two sum

--- two_sum/solution.py
+++ two_sum/solution.py
@@ -5,4 +5,26 @@ class Solution:
         for i, e in enumerate(num):
             if e in d:
                 return d[e] + 1, i + 1
-            d[target - e] = i
+        d[target - e] = i
+
+
+
+def twoSum(num, target):
+    d = {}
+    for i, e in enumerate(num):
+        if e in d:
+            return d[e] + 1, i + 1
+        d[target - e] = i
+        print(d)
+
+
+
+def mytwoSum(num, target):
+    numlen = len(num)
+    for i in range(numlen):
+        for j in range(i+1,numlen):
+            if target == (num[i]+num[j]):
+                return i+1,j+1
+
+
+
