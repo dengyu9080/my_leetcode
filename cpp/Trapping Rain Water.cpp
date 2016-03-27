8d298cf8cbc88093eb5782363788f1f33023d1dd
Qiang Wang <wangqiang2015@ia.ac.cn>
2016-03-27 16:35:54 +0800

    Trapping Rain Water
    21.81%

--- cpp/Trapping Rain Water.cpp
+++ cpp/Trapping Rain Water.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-               int result = 0;
+              int result = 0;
     
     if (height.size()<2)return 0;
     
-    vector<int>llevel = {height[0]};
-    vector<int>rlevel = {*(--height.end())};
     
-    for (int i = 1 ; i<height.size(); i++) {
+    vector<int>llevel = height;
+    
+    int i = 1;
+    
+    for (; i<height.size()-1; i++) {
         if (height[i]>=llevel[i-1])
-            llevel.push_back(height[i]);
+            llevel[i] = height[i];
         else
-            llevel.push_back(llevel[i-1]);
+            llevel[i] = llevel[i-1];
+    }
+    llevel[i] = height[i];
+    
+    for (i--; i!=0; i--) {
+        if (height[i]>=llevel[i+1])
+            llevel[i] = min(height[i],llevel[i]);
+        else
+            llevel[i] = min(llevel[i+1],llevel[i]);
     }
     
-    for (int i = int(height.size())-2 ; i>=0; i--) {
-        
-        if (height[i]>=rlevel[int(height.size())-2-i])
-            rlevel.push_back(height[i]);
-        else
-            rlevel.push_back(rlevel[int(height.size())-2-i]);
-    }
-    for (int i = 0 ; i<height.size(); i++)
-        result = result+(llevel[i]>rlevel[height.size()-1-i]?(rlevel[height.size()-1-i]-height[i]):(llevel[i]-height[i]));
+    for (auto x:llevel)
+        result +=x;
+    for (auto x:height)
+        result -=x;
+
     
     return result;
     }
