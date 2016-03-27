0884f66b7357d2513181a4a183a46e4cf4a2318c
Qiang Wang <wangqiang2015@ia.ac.cn>
2016-03-27 18:10:00 +0800

    Climbing Stairs
    3.43%

--- cpp/Climbing Stairs.cpp
+++ cpp/Climbing Stairs.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n>2)
-        return climbStairs(n-1)+climbStairs(n-2);
-        else return n;
+            if (n<3)return n;
+    else
+    {
+        int step1 = 1;
+        int step2 = 2;
+        int temp;
+        for (int i =3; i<=n; i++) {
+            temp = step2;
+            step2 = step1+step2;
+            step1 = temp;
+        }
+        return step2;
+    }
     }
 };
