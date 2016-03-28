acbd50196bc1cd4d420e89bb12c2664bfec64356
Qiang Wang <wangqiang2015@ia.ac.cn>
2016-03-28 10:03:04 +0800

    Set Matrix Zeroes
    S(n) = O(1),21.60%

--- cpp/Set Matrix Zeroes.cpp
+++ cpp/Set Matrix Zeroes.cpp
@@ -4,28 +4,34 @@ public:
         int m = int(matrix.size());
     int n = int(matrix[0].size());
     
+    int zerocolflag = 1;
+    for (auto x:matrix[0]){
+        if (x==0) {
+            zerocolflag =0;
+        }
+    }
+
     
-    
-    vector<int>colDot(n,1);
-    vector<int>rowDot(m,1);
-    for(int i = 0;i<m;i++){
+    for(int i = 1;i<m;i++){
         for (int j =0 ; j<n; j++) {
-            colDot[j] = colDot[j]*(matrix[i][j]!=0);
-            rowDot[i] = rowDot[i]*(matrix[i][j]!=0);
+            matrix[0][j] = matrix[0][j]*(matrix[i][j]!=0);
+            matrix[i][0] = matrix[i][0]*(matrix[i][j]!=0);
         }
     }
     
+    for (int i = 1; i<m; i++) {
+        if (matrix[i][0]==0)
+            fill(matrix[i].begin(), matrix[i].end(), 0);
+    }
+    
     for (int i = 0; i<n; i++) {
-        if (colDot[i]==0) {
+        if (matrix[0][i]==0) {
             for (int j = 0; j<m; j++)
                 matrix[j][i] = 0;
         }
     }
-    for (int i = 0; i<m; i++) {
-        if (rowDot[i]==0) {
-            for (int j = 0; j<n; j++)
-                matrix[i][j] = 0;
-        }
+    if (zerocolflag==0) {
+        fill(matrix[0].begin(), matrix[0].end(), 0);
     }
     }
 };
