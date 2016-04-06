f9afbafedc947ae4b8344b85c9ba5777aee7f050
Qiang Wang <wangqiang2015@ia.ac.cn>
2016-04-06 17:22:29 +0800

    Longest Palindromic Substring
    16.41% DP T(n) = O(n^2)  different data structure have different speed!
    sometimes *?*:*; is not fast!

--- cpp/Longest Palindromic Substring.cpp
+++ cpp/Longest Palindromic Substring.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-      int maxLen = 1;
-    string result;
+       int maxLen = 1;
     int nlen = int(s.length());
-    vector<vector<bool>>f(nlen,vector<bool>(nlen,false));
+    bool f[nlen][nlen];
     
     int start = 0;
     
     for (int i = 0; i<nlen; i++) {
         f[i][i] = true;
         for (int j = 0; j<i; j++) {
-            f[j][i] = s[i]==s[j]&&(i-j==1||f[j+1][i-1])?maxLen<i-j+1?maxLen = i-j+1,start = j,true:true:0;
+            // f[j][i] = s[i]==s[j]&&(i-j==1||f[j+1][i-1])?maxLen<i-j+1?maxLen = i-j+1,start = j,true:true:0;
+            f[j][i] = s[i]==s[j]&&(i-j==1||f[j+1][i-1]);
+            if (f[j][i]&&maxLen<i-j+1) {
+                maxLen = i-j+1;
+            
+            
+                start = j;
+            }
         }
     }
-    
-    
-    result = s.substr(start,maxLen);
-    
-    
-    return result;
+    return s.substr(start,maxLen);
     }
 };
