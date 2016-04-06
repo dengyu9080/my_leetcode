225384c95b7164c25f40053d330387d9dd36eafa
Qiang Wang <wangqiang2015@ia.ac.cn>
2016-04-06 16:47:50 +0800

    Longest Palindromic Substring
    TLE(recursion)

--- cpp/Longest Palindromic Substring.cpp
+++ cpp/Longest Palindromic Substring.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int maxLen = 0;
+      int maxLen = 0;
+    vector<int> maxId(2,0);
     string result;
-    for (int i = 0; i<s.length(); i++) {
-        for (int j = i; j<s.length(); j++) {
-            bool flag = true;
-            for (int k = 0; k<=(j-i)/2; k++) {
-//                cout<<s[i+k]<<s[j-k]<<endl;
-                if(s[i+k]!=s[j-k])
-                {
-                    flag = false;
-                    break;
-                }
+    int nlen = int(s.length());
+    vector<vector<int>>f(nlen,vector<int>(nlen,0));
+    
+    for (int i = 0; i<nlen; i++) {
+        for (int j = i; j<nlen; j++) {
+            f[i][j] =  longestP(s,i,j);
+            if (f[i][j]>maxLen) {
+                maxLen = f[i][j];
+                result = s.substr(i,j-i+1);
             }
-            
-            if (flag &&(j-i+1)>maxLen) {
-                result = s.substr(i,(j-i+1));
-                maxLen = j-i+1;
-            }
-            
         }
     }
     
     return result;
     }
+private:
+int longestP(string s,int i,int j)
+{
+    if (i==j) return 1;
+    else if(i==j-1&&s[i]==s[j]) return 2;
+    else if(s[i]==s[j]&&(i+1)<=(j-1) &&longestP(s,i+1,j-1)== j-i-1)
+        return j-i+1;
+    else
+        return 0;
+}
 };
