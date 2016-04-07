ea18511d5d1bd31c2a767eddb616f77fee2f9935
Qiang Wang <wangqiang2015@ia.ac.cn>
2016-04-07 15:49:03 +0800

    Merge Two Sorted Lists
    5.30%(I want to run fast,but fail)

--- cpp/Merge Two Sorted Lists.cpp
+++ cpp/Merge Two Sorted Lists.cpp
@@ -9,46 +9,32 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if (!l1) {
-        return l2;
-    }
-    if (!l2) {
-        return l1;
-    }
+    if (!l1) return l2;
+    if (!l2) return l1;
     ListNode *ihead;
-    
     if (l1->val<l2->val) {
         ihead = l1;
         l1 = l1->next;
     }
-    else
-    {
+    else {
         ihead = l2;
         l2 = l2->next;
     }
-    
-    
     ListNode *icur = ihead;
-    
     while (l1&&l2) {
         if (l1->val>l2->val) {
             icur->next = l2;
             l2 = l2->next;
             icur = icur->next;
         }
-        else
-        {
+        else {
             icur->next = l1;
             l1 = l1->next;
             icur = icur->next;
         }
     }
     
-    if (!l1) {
-        icur->next = l2;
-    }
-    else
-        icur->next = l1;
+    icur->next = l1?l1:l2;
     
     return ihead;
     }
