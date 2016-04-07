f17a2444378225a62106b09493e67247af047e7f
Qiang Wang <wangqiang2015@ia.ac.cn>
2016-04-07 09:37:39 +0800

    Merge Two Sorted Lists
    5.30%

---
+++ cpp/Merge Two Sorted Lists.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (!l1) {
+        return l2;
+    }
+    if (!l2) {
+        return l1;
+    }
+    ListNode *ihead;
+    
+    if (l1->val<l2->val) {
+        ihead = l1;
+        l1 = l1->next;
+    }
+    else
+    {
+        ihead = l2;
+        l2 = l2->next;
+    }
+    
+    
+    ListNode *icur = ihead;
+    
+    while (l1&&l2) {
+        if (l1->val>l2->val) {
+            icur->next = l2;
+            l2 = l2->next;
+            icur = icur->next;
+        }
+        else
+        {
+            icur->next = l1;
+            l1 = l1->next;
+            icur = icur->next;
+        }
+    }
+    
+    if (!l1) {
+        icur->next = l2;
+    }
+    else
+        icur->next = l1;
+    
+    return ihead;
+    }
+};
