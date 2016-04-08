5cdb9c6f714ec49efb47104f3e1f95080e6fea87
Qiang Wang <wangqiang2015@ia.ac.cn>
2016-04-08 10:41:39 +0800

    Search Insert Position
    beauty STL

--- cpp/Search Insert Position.cpp
+++ cpp/Search Insert Position.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-              int a = 0,b = int(nums.size()),c=0;
-    while(a<b) {
-        c = (a+b)/2;
-        if (nums[c]==target) {
-            return c;
-        }
-        else if(nums[c]<target)
-            a = c+1;
-        else
-            b = c;
-    }
-    return b;
+       return lower_bound(nums.begin(),nums.end(),target)-nums.begin();
     }
 };
