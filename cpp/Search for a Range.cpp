623aa1d9d4b787f0955149ebf46de67e118cf14d
Qiang Wang <wangqiang2015@ia.ac.cn>
2016-04-08 10:18:07 +0800

    Search for a Range
    beauty STL

--- cpp/Search for a Range.cpp
+++ cpp/Search for a Range.cpp
@@ -1,36 +1,11 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-      int nlen = int(nums.size());
-    int a = 0,b = nlen,c = a;
-    vector<int>result(2,-1);
-    for (a =0,b =nlen; a<=b; ) {
-        c = (a+b)/2;
-        if (nums[c]<target) {
-            a = c+1;
-        }
-        else if(nums[c]>target)
-            b = c-1;
-        else{
-            result[0] = c;
-            b = c-1;
-        }
-    }
-    
-    if (result[0] !=-1) {
-        for (a =result[0],b =nlen-1; a<=b; ) {
-            c = (a+b)/2;
-            if (nums[c]>target) {
-                b = c-1;
-            }
-            else if(nums[c]<target)
-                a = c+1;
-            else{
-                result[1] = c;
-                a = c+1;
-            }
-        }
-    }
-    return result;
+    int l = int(distance(nums.begin(),lower_bound(nums.begin(),nums.end(),target)));
+    int r = int(distance(nums.begin(),prev(upper_bound(nums.begin(),nums.end(),target))));
+    if (nums[l]!=target) 
+        return vector<int>{-1,-1};
+    else
+        return vector<int>{l,r};
     }
 };
