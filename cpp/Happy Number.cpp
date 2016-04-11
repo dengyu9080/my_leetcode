a2904ee1c0982366fd81c48cefd9f659762e8bc4
Qiang Wang <wangqiang2015@ia.ac.cn>
2016-04-11 14:16:04 +0800

    Happy Number
    97.40%(my own way!!!)

--- cpp/Happy Number.cpp
+++ cpp/Happy Number.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        if (n<=0) {
+        int mychange[10]={0,1,4,9,16,25,36,49,64,81};
+    bool happy[100]={0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,1,0,0,0,0,1,0,0,1,0,0,1,0,0};
+    if (n<=0) {
         return false;
     }
     else
     {
         while (1) {
-            if (n<10) {
-                if(n!=1&&n!=7)
-                    return false;
-                else
-                    return true;
-            }
+            if (n<100) return happy[n];
+            
             int temp = 0;
-            while ((n/10)!=0) {
-                temp = temp+(n%10)*(n%10);
+            while (n>0) {
+                temp = temp+mychange[n%10];
                 n = n/10;
             }
-            n =  temp+n*n;
-            
+            n = temp;
         }
     }
-        
     }
 };
